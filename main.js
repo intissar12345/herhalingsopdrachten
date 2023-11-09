3a8cd86528a984ac6ad884962a1c26e0c0baedd3
intissar12345 <144015814+intissar12345@users.noreply.github.com>
2023-11-09 12:53:54 +0100

    opdr 2

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 console.log("hallo");
 
+// opdracht 1
 let salery=2000;
 let percent=0.05;
 
 let raise=salery*percent;
 console.log("verhoging is ",raise);
+
+// opdracht 2
+let hypotheek=400000;
+let rente=0.025;
+let prijs=hypotheek*rente;
+console.log("rente per  jaar is",prijs);
